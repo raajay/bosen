70fb0cf7bb43a4d98561954819b64f92bb3891a8
raajay <raajay@cs.wisc.edu>
2017-02-17 15:44:08 -0600

    fix the issue with system flags

--- src/petuum_ps_common/include/system_gflags.hpp
+++ src/petuum_ps_common/include/system_gflags.hpp
@@ -5,45 +5,38 @@
 
 #include <petuum_ps_common/include/configs.hpp>
 #include <petuum_ps_common/util/utils.hpp>
+#include <cstdint>
 
-DEFINE_string(stats_path, "", "stats file path prefix");
+DECLARE_string(stats_path);
 
-// Topology Configs
-DEFINE_int32(num_clients, 1, "total number of clients");
-DEFINE_int32(num_comm_channels_per_client, 1, "no. of comm channels per client");
-DEFINE_bool(init_thread_access_table, false, "whether init thread accesses table");
-DEFINE_int32(num_table_threads, 1, "no. of worker threads per client");
-DEFINE_int32(client_id, 0, "This client's ID");
-DEFINE_string(hostfile, "", "path to Petuum PS server configuration file");
+DECLARE_int32(num_clients);
+DECLARE_string(hostfile);
+DECLARE_int32(num_comm_channels_per_client);
+DECLARE_bool(init_thread_access_table);
+DECLARE_int32(num_table_threads);
+DECLARE_int32(client_id);
+DECLARE_int32(num_app_threads);
 
-// Execution Configs
-DEFINE_string(consistency_model, "SSP", "SSP");
+DECLARE_string(consistency_model);
+DECLARE_uint64(bandwidth_mbps);
+DECLARE_uint64(bg_idle_milli);
+DECLARE_uint64(oplog_push_upper_bound_kb);
+DECLARE_int32(oplog_push_staleness_tolerance);
+DECLARE_uint64(thread_oplog_batch_size);
 
-// SSPAggr Configs -- client side
-DEFINE_uint64(bandwidth_mbps, 400, "per-thread bandwidth limit, in mbps");
-//DEFINE_uint64(bg_idle_milli, 10, "Bg idle millisecond");
-DEFINE_uint64(bg_idle_milli, 0, "Bg idle millisecond");
+DECLARE_uint64(server_row_candidate_factor);
+DECLARE_int32(server_push_row_threshold);
+DECLARE_int32(server_idle_milli);
+DECLARE_string(update_sort_policy);
 
-DEFINE_uint64(oplog_push_upper_bound_kb, 1000,
-             "oplog push upper bound in Kilobytes per comm thread.");
-DEFINE_int32(oplog_push_staleness_tolerance, 2,
-             "oplog push staleness tolerance");
-DEFINE_uint64(thread_oplog_batch_size, 100*1000*1000, "thread oplog batch size");
+DECLARE_int32(snapshot_clock);
+DECLARE_int32(resume_clock);
+DECLARE_string(snapshot_dir);
+DECLARE_string(resume_dir);
 
-// SSPAggr Configs -- server side
-DEFINE_uint64(server_row_candidate_factor, 5, "server row candidate factor");
-DEFINE_int32(server_push_row_threshold, 100, "Server push row threshold");
-//DEFINE_int32(server_idle_milli, 10, "server idle time out in millisec");
-DEFINE_int32(server_idle_milli, 0, "server idle time out in millisec");
-DEFINE_string(update_sort_policy, "Random", "Update sort policy");
-
-// Snapshot Configs
-DEFINE_int32(snapshot_clock, -1, "snapshot clock");
-DEFINE_int32(resume_clock, -1, "resume clock");
-DEFINE_string(snapshot_dir, "", "snap shot directory");
-DEFINE_string(resume_dir, "", "resume directory");
 namespace petuum {
 
 void InitTableGroupConfig(TableGroupConfig *config, int32_t *client_id,
                           int32_t num_tables);
 }
+
